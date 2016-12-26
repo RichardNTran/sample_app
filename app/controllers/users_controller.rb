03d49d4e40a02eb76590b7cf2ed1df036dd78881
RichardNTran <trannguyenlong1991@gmail.com>
2016-12-26 17:01:26 +0700

    up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  def new
-  end
+	def new
+		@user=User.new
+		
+	end
+	def create
+		@user=User.new(user_params)
+		if @user.save
+			debugger
+			redirect_to user_url(@user) 
+		else
+			render 'new'
+		end
+	end
+	def show 
+		@user=User.find(params[:id])
+		 # debugger
+		end
+
+		private	def user_params
+		params.require(:user).permit(:name, :email, :password,:password_confimation)
+	end
+
 end
